ec26778ade6593ac58ef3ce6fa37b865fdfb0d1c
Ruah Kim <kimhk0315@gmail.com>
2024-11-13 22:04:47 +0900

    gpt 응답 프린트

--- finetuning.py
+++ finetuning.py
@@ -78,10 +78,13 @@ if status.status == "succeeded":
     messages = [
         {
             "role": "user",
-            "content": "당뇨환자가 식당에서 먹어도 되는 음식 종류를 골라주세요",
+            # "content": "당뇨환자가 식당에서 먹어도 되는 음식 종류를 골라주세요",
+            "content": "비빔밥 영양성분이 어떻게 되나요",
         }
     ]
     completion = openai.chat.completions.create(
         model=fine_tuned_model, messages=messages, max_tokens=300
     )
     print(f"Model Response : {completion.choices[0].message.content.strip()}")
+
+# Model Response : 중탄고지 식단 영양소비율 기준에 따르면 한 그릇(510.5g)의 비빔밥 영양성분은 에너지 650.62kcal, 탄수화물 121.04399999999998g, 당류 7.819g, 지방 10.388000000000002g, 단백질 19.795999999999994g, 칼슘 65.89999999999999mg, 나트륨 1328.93mg, 인 321.23mg, 칼륨 606.6mg, 마그네슘 32.375mg, 철 4.669999999999999mg, 아연 2.8935mg, 콜레스테롤 22.08mg, 트랜스지방 0g  입니다. 부엌에서 조리한 7,000여종의 식품 중 영양가가 높습니다.
