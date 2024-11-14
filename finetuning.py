9ff47e7ba696939508d6b2e622f375143d36d425
Ruah Kim <kimhk0315@gmail.com>
2024-11-14 21:38:44 +0900

    주석 수정

--- finetuning.py
+++ finetuning.py
@@ -2,36 +2,6 @@ from init import *
 import pandas as pd
 import json
 
-excel_file_path = "/Users/ruahkim/coding/ai-test/openai-fine-tuning/docs/44.음식분류_AI_데이터_영양DB.xlsx"
-
-
-# 데이터셋 로드
-# df = pd.read_excel(excel_file_path, engine="openpyxl")
-
-# # # 데이터셋을 json 형식으로 변환하여 저장
-# with open("health_food_data.jsonl", "w") as f:
-#     for _, row in df.iterrows():
-#         prompt = f"{row['음 식 명']}의 칼로리와 영양 정보를 알려주세요."
-
-#         # completion에 영양 정보 포함한 응답 생성
-#         completion = (
-#             f"중량: {row['중량(g)']}g, 에너지: {row['에너지(kcal)']}kcal, "
-#             f"탄수화물: {row['탄수화물(g)']}g, 당류: {row['당류(g)']}g, "
-#             f"지방: {row['지방(g)']}g, 단백질: {row['단백질(g)']}g, "
-#             f"칼슘: {row['칼슘(mg)']}mg, 나트륨: {row['나트륨(mg)']}mg, "
-#             f"인: {row['인(mg)']}mg, 칼륨: {row['칼륨(mg)']}mg, "
-#             f"마그네슘: {row['마그네슘(mg)']}mg, 철: {row['철(mg)']}mg, "
-#             f"아연: {row['아연(mg)']}mg, 콜레스테롤: {row['콜레스테롤(mg)']}mg, "
-#             f"트랜스지방: {row['트랜스지방(g)']}g"
-#         )
-#         chat_data = {
-#             "messages": [
-#                 {"role": "user", "content": prompt},
-#                 {"role": "assistant", "content": completion},
-#             ]
-#         }
-#         f.write(json.dumps(chat_data, ensure_ascii=False) + "\n")
-
 
 file_path = (
     "/Users/ruahkim/coding/ai-test/openai-fine-tuning/docs/health_food_data.jsonl"
@@ -49,7 +19,7 @@ df_jsonl = pd.read_json(
 # 파인튜닝 작업 id 확인
 # file_id = response.id
 # print(f"Uploaded file ID: {file_id}")
-file_id = "file-file-qGjAqVzbmSj4eFRD6m4xRxw0"
+file_id = "file-qGjAqVzbmSj4eFRD6m4xRxw0"
 # 파일 삭제
 # openai.files.delete(file_id=file_id)
 
@@ -79,11 +49,11 @@ if status.status == "succeeded":
         {
             "role": "user",
             # "content": "당뇨환자가 식당에서 먹어도 되는 음식 종류를 골라주세요",
-            "content": "비빔밥 영양성분이 어떻게 되나요",
+            "content": "비빔밥의 영양성분은 어떻게 되나요?",
         }
     ]
     completion = openai.chat.completions.create(
-        model=fine_tuned_model, messages=messages, max_tokens=300
+        model=fine_tuned_model, messages=messages, max_tokens=500, temperature=0.98
     )
     print(f"Model Response : {completion.choices[0].message.content.strip()}")
 
